340126b5f07b97aff73722c04a48adfb8e124838
AniPortal.live | Dev <nanunema123@gmail.com>
2025-10-15 15:49:48 +0200

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -1,1 +1,8 @@
-
+export function setupControls(camera, renderer){
+  const controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+  controls.minDistance = 20;
+  controls.maxDistance = 150;
+  return controls;
+}
